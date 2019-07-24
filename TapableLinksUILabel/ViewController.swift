86e06d5ee76e762588b60fcf06d22d0cb407e79e
Aman Aggarwal <aman@clickapps.co>
2019-07-24 20:11:33 +0530

    Initial Commit

---
+++ TapableLinksUILabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapableLinksUILabel
+//
+//  Created by Aman Aggarwal on 24/07/19.
+//  Copyright © 2019 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
